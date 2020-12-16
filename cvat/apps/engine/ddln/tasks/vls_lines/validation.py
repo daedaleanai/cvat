fd786967fc3a3ee307b395ab6e82eb4264f5f614
Alexander Gerbik <alexg@daedalean.ai>
2020-12-16 21:15:11 +0300

    Warn when there are multiple runway ids in the same sequence

--- cvat/apps/engine/ddln/tasks/vls_lines/validation.py
+++ cvat/apps/engine/ddln/tasks/vls_lines/validation.py
@@ -5,8 +5,19 @@ def validate(sequences, reporter=None, **kwargs):
     if reporter is None:
         reporter = VlsLinesValidationReporter()
     for seq in sequences:
+        reporter.sequence = seq.name
+        previous_id = None
+
         for frame in seq.frames:
+            reporter.frame = frame.name
+            reporter.frame_index = frame.index
             reporter.count_frame(seq.name)
+
+            for runway in frame.objects:
+                current_id = runway.id
+                if previous_id and current_id != previous_id:
+                    reporter.report_id_changed(previous_id, current_id)
+                previous_id = current_id
     return reporter
 
 
@@ -24,6 +35,9 @@ class VlsLinesValidationReporter(BaseValidationReporter):
         type = "longitudinal" if is_lon else "lateral"
         severity = self.severity.ERROR if is_lon else self.severity.WARNING
         self._report("Runway {!r} lacks {} rays".format(runway_id, type), severity)
+
+    def report_id_changed(self, previous, current):
+        self._report("Runway id has changed from {} to {}".format(previous, current))
 
     def report_lat_disorder(self):
         self._report("Lateral rays are mixed up")
