00e14a59aeca425a9bf780baab2beba45f519a9c
Alexander Gerbik <alexg@daedalean.ai>
2021-02-01 15:58:53 +0300

    Refactor: move invisible lines guessing to csv module
    Because it addresses csv import issue, not cvat export issue.

--- cvat/apps/engine/ddln/tasks/vls_lines/persistence/cvat.py
+++ cvat/apps/engine/ddln/tasks/vls_lines/persistence/cvat.py
@@ -1,4 +1,4 @@
-from cvat.apps.engine.ddln.geometry import Line, get_angle_between, Point
+from cvat.apps.engine.ddln.geometry import Line, Point
 from cvat.apps.engine.utils import grouper
 from ..models import Runway
 from ...utils import build_attrs_dict
@@ -107,14 +107,10 @@ def _parse_lines(shape):
 
 
 def _get_points_list(runway):
-    # Coordinates for non-visible lines are not stored in csv,
-    # so have to guess them
-    lon_lines = _guess_lines(runway.lon_vanishing_point, runway.left_line, runway.center_line, runway.right_line)
-    lat_lines = _guess_lines(runway.lat_vanishing_point, runway.start_line, runway.designator_line, runway.end_line)
     intersections = []
-    for a in lon_lines:
+    for a in (runway.left_line, runway.center_line, runway.right_line):
         row = []
-        for b in lat_lines:
+        for b in (runway.start_line, runway.designator_line, runway.end_line):
             row.append(a.intersect(b))
         intersections.append(row)
 
@@ -136,59 +132,6 @@ def _get_points_list(runway):
         lat_result.append(point.x)
         lat_result.append(point.y)
     return lon_result, lat_result
-
-
-def _guess_lines(vanishing_point, first, second, third):
-    missing_lines_amount = sum(line is None for line in [first, second, third])
-    if missing_lines_amount == 0:
-        return first, second, third
-    if missing_lines_amount == 1:
-        if vanishing_point:
-            if not first:
-                dphi = get_angle_between(second.get_angle(), third.get_angle())
-                first = Line.by_point_and_angle(vanishing_point, second.get_angle() - dphi)
-            elif not second:
-                dphi = get_angle_between(first.get_angle(), third.get_angle()) / 2
-                second = Line.by_point_and_angle(vanishing_point, first.get_angle() + dphi)
-            elif not third:
-                dphi = get_angle_between(first.get_angle(), second.get_angle())
-                third = Line.by_point_and_angle(vanishing_point, second.get_angle() + dphi)
-        else:
-            if not first:
-                dx = third.c - second.c
-                first = Line(second.a, second.b, second.c - dx)
-            elif not second:
-                dx = (third.c - first.c) / 2
-                second = Line(third.a, third.b, third.c - dx)
-            elif not third:
-                dx = second.c - first.c
-                third = Line(second.a, second.b, second.c + dx)
-        return first, second, third
-    if missing_lines_amount == 2:
-        if vanishing_point:
-            dphi = 0.175
-            if first:
-                second = Line.by_point_and_angle(vanishing_point, first.get_angle() + dphi)
-                third = Line.by_point_and_angle(vanishing_point, second.get_angle() + dphi)
-            elif second:
-                first = Line.by_point_and_angle(vanishing_point, second.get_angle() - dphi)
-                third = Line.by_point_and_angle(vanishing_point, second.get_angle() + dphi)
-            elif third:
-                second = Line.by_point_and_angle(vanishing_point, third.get_angle() - dphi)
-                first = Line.by_point_and_angle(vanishing_point, second.get_angle() - dphi)
-        else:
-            dx = 10
-            if first:
-                second = Line(first.a, first.b, first.c + dx)
-                third = Line(second.a, second.b, second.c + dx)
-            elif second:
-                first = Line(second.a, second.b, second.c - dx)
-                third = Line(second.a, second.b, second.c + dx)
-            elif third:
-                second = Line(third.a, third.b, third.c - dx)
-                first = Line(second.a, second.b, second.c - dx)
-        return first, second, third
-    raise RunwayParseError("All lines cannot be invisible")
 
 
 def _get_attributes(runway):
